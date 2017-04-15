4dcefeec00bde7ac7e430db342e659d1592f15bc
Jake <cactusbat@hotmail.com>
2017-04-14 18:57:13 -0700

    Create pickPocket.js

---
+++ pickPocket.js
@@ -0,0 +1,19 @@
+module.exports = (pickPocket) => {
+// const trinket = ["Piece of crystal","Foreign gold coin","Brass ring","An old glass chess piece",\
+// "Pair of bone dice","A rope necklace which dangles a mummified" + finger]
+//  let finger = ["high elf","drow","dwarf","human","dragonborne","halfling","gnome","demon",
+//  "angel","rabbit's foot","half orc", "orc",]
+  const gold = function () {
+    let goldPieces = function getRandomIntInclusive(min, max) {
+      min = Math.ceil(1);
+      max = Math.floor(20);
+      return Math.floor(Math.random() * (max - min) + 1) + min;
+    }
+    goldpieces + 'gold pieces.';
+  };
+
+// let pickPocket = function(gold, trinket) {
+// };
+gold
+  console.log(gold + 'gold pieces.');
+};
